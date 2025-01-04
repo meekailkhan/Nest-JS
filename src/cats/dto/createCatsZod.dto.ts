deafdc9912162b53b321094214378fd21dfad113
pc <meekailkhan123aar@gmail.com>
2025-01-04 23:06:50 +0530

    nest js

--- src/cats/dto/createCatsZod.dto.ts
+++ src/cats/dto/createCatsZod.dto.ts
@@ -3,7 +3,13 @@ export const createCatSchema = z.object({
     name : z.string().min(3).max(12),
     description : z.string().min(5).max(20),
     age : z.number().positive().min(1).max(25)
-})
+}).strict().refine((data) => {
+    const allowedKeys = ['name', 'age','description'];
+    const extraKeys = Object.keys(data).filter((key) => !allowedKeys.includes(key));
+    return extraKeys.length === 0;
+  }, {
+    message: 'Object contains forbidden keys.',
+  });
 
 
 export type createCatsZodDto = z.infer<typeof createCatSchema>
